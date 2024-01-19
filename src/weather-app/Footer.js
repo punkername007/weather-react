150319334ffd14584bc32d8c1224a2dfa4038cfa
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-19 04:20:27 +0100

    fixed links

--- src/weather-app/Footer.js
+++ src/weather-app/Footer.js
@@ -7,17 +7,17 @@ export default function Footer() {
       <footer>
         <p>
           This project was coded by
-          <a href="#" target="_blank">
+          <a href="#" target="_blank" rel="noreferrer">
             {" "}
             Selene Roa
           </a>{" "}
           and is
-          <a href="#" target="_blank">
+          <a href="#" target="_blank" rel="noreferrer">
             {" "}
             on GitHub
           </a>{" "}
           and
-          <a href="#" target="_blank">
+          <a href="#" target="_blank" rel="noreferrer">
             {" "}
             hosted on Netlify
           </a>
